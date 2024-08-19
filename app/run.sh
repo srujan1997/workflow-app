bde78fc07732458871a35cebdbad0c9e9b45ced2
Srujan <srujan1997@gmail.com>
2024-08-18 20:43:47 -0400

    update run.sh

--- app/run.sh
+++ app/run.sh
@@ -1,1 +1,19 @@
+#!/bin/bash
+
+# Get the current working directory
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+
+# Create a virtual environment if it doesn't exist
+if [ ! -d "$DIR/../venv" ]; then
+    python3 -m venv "$DIR/../venv"
+    echo "Virtual environment created."
+fi
+
+# Activate the virtual environment
+source "$DIR/../venv/bin/activate"
+
+# Install the requirements
+pip install -r "$DIR/requirements.txt"
+
 exec python3 wsgi.py
